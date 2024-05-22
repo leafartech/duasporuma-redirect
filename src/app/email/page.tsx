9cc348aa121ae6bd7f94576b71c540523c9c4b23
leafartech <119262053+leafartech@users.noreply.github.com>
2024-05-21 22:27:54 -0300

    Initial Commit

---
+++ src/app/email/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
+
+export default function Page() {
+    const router = useRouter()
+
+    useEffect(() => {
+        router.push('https://duasporuma.com.br/pesquisa/?sck=&utm_source=email&utm_medium=06.24&utm_content=emailpesquisa&utm_campaign=06.24&utm_term=06.24')
+    }, [])
+
+    return (
+        <></>
+    )
+}
